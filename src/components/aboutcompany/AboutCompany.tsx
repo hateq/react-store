2210d2c1b4038d43eeec26b172871ca2cbaca480
unknown <konyukhov.vad@gmail.com>
2024-01-23 23:06:52 +0300

    b

--- src/components/aboutcompany/AboutCompany.tsx
+++ src/components/aboutcompany/AboutCompany.tsx
@@ -4,7 +4,7 @@ const AboutCompany = () => {
 	return ( 
 		<div className="about-company">
 			<h2>About us</h2>
-			<p>Our company was created in 1703 year by Petr the first in Krasnodar, Russia. First all that we did was just cooking very tasty pancakes, but then in the 19 century, when it was very populary, we transformed into the enternet shop. Now we have over 1 million users per month. Ok, maybe you think that this is some nonsense. Yes, this is fake that i created for my pet project. If you read this you can go to my github "hateq". Hope you enjoy the website</p>
+			<p>!Our company was created in 1703 year by Petr the first in Krasnodar, Russia. First all that we did was just cooking very tasty pancakes, but then in the 19 century, when it was very populary, we transformed into the enternet shop. Now we have over 1 million users per month. Ok, maybe you think that this is some nonsense. Yes, this is fake that i created for my pet project. If you read this you can go to my github "hateq". Hope you enjoy the website</p>
 		</div>
 	 );
 }
